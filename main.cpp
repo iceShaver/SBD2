a14a355e456a2dc6f7285364dcdb53c50628ea25
kamil <kamil-krolikowski@outlook.com>
2018-12-17 22:24:20 +0100

    refactor

--- main.cpp
+++ main.cpp
@@ -10,23 +10,11 @@ int main(int argc, char **argv) {
 
 /*
  * Stuff to implement:
- * * removing records
+ * * removing records and add it to test files
  * !* reverse iterator
- * !* reading data from Test files (CUD operations) done
  *
- *params of db: I/O pages counter, height, nodes number, size, records number, % disk and ram memory utilization (disk util will be good for an experiment)
  * !*
  * !* experiment
- * *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  *
  * Stuff to improve:
  * * AllocateDiskMemory (use some array of free spaces??)
